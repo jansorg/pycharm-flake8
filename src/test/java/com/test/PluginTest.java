df6d96c96c8d0547bb2b2a8292eb3a8135aac088
jansorg <github@joachim-ansorg.de>
2019-01-11 14:05:31 +0100

    update to current versions cleanup

--- src/test/java/com/test/PluginTest.java
+++ src/test/java/com/test/PluginTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
  */
 public class PluginTest {
     @Test
-    public void testBasics() throws Exception {
+    public void testBasics() {
         Assert.assertTrue("All is well", true);
     }
 }
